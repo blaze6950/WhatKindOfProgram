00b0d60d2a0eb2c9fd8b8a82534ddb00df94b33b
blaze6950 <d.progect21@gmail.com>
2018-06-30 14:36:47 +0300

    Created GUI

--- WhatKindOfProgram/MainWindow.xaml.cs
+++ WhatKindOfProgram/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WhatKindOfProgram
 {
@@ -23,6 +23,7 @@ namespace WhatKindOfProgram
         public MainWindow()
         {
             InitializeComponent();
+            //Path.
         }
     }
 }
